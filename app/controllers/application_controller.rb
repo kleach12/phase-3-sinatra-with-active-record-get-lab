1ac8b7531b9430a7fb9de19a899f134ca89cb9cd
Kale Leach <kleach1212@gmail.com>
2022-07-13 18:27:21 -0700

    commit tree b39eba4097b998c4a66d5aa1d6942c8cab8785c1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,43 @@
 class ApplicationController < Sinatra::Base
 
-  # add routes
+  set :default_content_type, 'application/json'
+
+  get '/bakeries' do
+
+    bakeries = Bakery.all
+
+    bakeries.to_json
+  end
+
+  get '/bakeries/:id' do
+
+    bakeries = Bakery.find(params[:id])
+
+    bakeries.to_json(include: :baked_goods)
+  end
+
+  get '/baked_goods' do
+
+    goods_by_price = BakedGood.all
+
+    goods_by_price.to_json
+
+  end
+
+  get '/baked_goods/by_price' do
+
+    goods_by_price = BakedGood.by_price
+
+    goods_by_price.to_json
+
+  end
+
+  get '/baked_goods/most_expensive' do
+
+    goods_by_price = BakedGood.by_price.first()
+
+    goods_by_price.to_json
+
+  end
 
 end
